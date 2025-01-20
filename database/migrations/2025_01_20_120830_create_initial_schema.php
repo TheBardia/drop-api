b6ddae98f9583d7978dddd10ce23eea6953edfab
TheBardia <bardiagoharanpour@rocketmail.com>
2025-01-20 14:10:05 +0100

    Changed schema to support the userID as UUID

--- database/migrations/2025_01_20_120830_create_initial_schema.php
+++ database/migrations/2025_01_20_120830_create_initial_schema.php
@@ -6,29 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
+
+        Schema::create('drops', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('user_id'); // Store UUID without foreign key
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+        
+        
+
         Schema::create('drop_files', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('drop_id');
             $table->string('original_name');
-            $table->string('aws_path');
             $table->bigInteger('size');
+            $table->string('aws_path');
             $table->timestamps();
-    
+
             $table->foreign('drop_id')->references('id')->on('drops')->onDelete('cascade');
         });
     }
-    
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('drop_files');
+        Schema::dropIfExists('drops');
     }
 };
